7c795e1d39fc68f22a289654f1e7b942a8557c59
Eric <eric.degomar@gmail.com>
2023-02-07 14:09:37 -0300

    layout finalizado, implementado viewBinding

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var binding : ActivityMainBinding
+    val calcClick = CalculateClickListener()
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        binding =  DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+            binding.number0.setOnClickListener {
+            Toast.makeText(this,"sera q foi", Toast.LENGTH_LONG).show()
+
+        }
+
+
     }
+
+
+
 }
