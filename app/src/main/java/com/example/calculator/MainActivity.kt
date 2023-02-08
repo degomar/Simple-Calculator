645d28b5eb9ee34764074433005a376099d44126
Eric <eric.degomar@gmail.com>
2023-02-08 16:02:51 -0300

    adicionados formatação para calculo matematico

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,25 +10,128 @@ import androidx.databinding.DataBindingUtil
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
-        override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var binding: ActivityMainBinding
+    var lastNumber: Boolean = false
+    var lastDot: Boolean = false
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //binding = ActivityMainBinding.inflate(layoutInflater)
-        binding =  DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
     }
-            fun onClickDigit(view: View){
-                binding.processCalc?.append((view as Button).text)
-                Toast.makeText(this, "clicou", Toast.LENGTH_LONG).show()
 
+    fun onClickDigit(view: View) {
+        binding.processCalc?.append((view as Button).text)
+        lastDot = false
+        lastNumber = true
+        Toast.makeText(this, "clicou", Toast.LENGTH_LONG).show()
+
+    }
+
+    fun onClear(view: View) {
+        binding.processCalc?.text = ""
+    }
+
+    fun onAllClear(view: View) {
+        onClear(view)
+        binding.calcResult?.text = ""
+    }
+
+    fun onDot(view: View) {
+        if (lastNumber && !lastDot) {
+            binding.processCalc?.append(".")
+            lastDot = true
+            lastNumber = false
+        }
+
+    }
+
+    fun onEquals(view: View) {
+        var tvValueString = binding.processCalc?.text.toString()
+        var resultSplits : Double? = null
+
+
+        try {
+            var prefix = ""
+            var firstValueSplited : String? = null
+            var secondValueSplited: String? = null
+
+            if (tvValueString.startsWith("-")){
+                prefix = "-"
+                tvValueString.substring(1)
+            }
+            if (tvValueString.contains("-")) {
+                var valueSplited = tvValueString.split("-")
+                 firstValueSplited = valueSplited[0]
+                 secondValueSplited = valueSplited[1]
+
+                if (prefix.isNotEmpty()) {
+                    firstValueSplited = prefix + firstValueSplited
                 }
 
-            fun onClear(view: View) {
-                binding.processCalc?.text = ""
-            }   fun onAllClear(view: View) {
-                onClear(view)
-                binding.calcResult?.text = ""
+                resultSplits = firstValueSplited.toDouble() - secondValueSplited.toDouble()
+            } else if (tvValueString.contains("+")) {
+                var valueSplited = tvValueString.split("+")
+                firstValueSplited = valueSplited[0]
+                secondValueSplited = valueSplited[1]
+
+                if (prefix.isNotEmpty()) {
+                    firstValueSplited = prefix + firstValueSplited
+                }
+
+                resultSplits = firstValueSplited.toDouble() + secondValueSplited.toDouble()
+            } else if (tvValueString.contains("/")) {
+                var valueSplited = tvValueString.split("/")
+                firstValueSplited = valueSplited[0]
+                secondValueSplited = valueSplited[1]
+
+                if (prefix.isNotEmpty()) {
+                    firstValueSplited = prefix + firstValueSplited
+                }
+
+                resultSplits = firstValueSplited.toDouble() / secondValueSplited.toDouble()
+            } else if (tvValueString.contains("x")) {
+                var valueSplited = tvValueString.split("x")
+                firstValueSplited = valueSplited[0]
+                secondValueSplited = valueSplited[1]
+
+                if (prefix.isNotEmpty()) {
+                    firstValueSplited = prefix + firstValueSplited
+                }
+
+                resultSplits = firstValueSplited.toDouble() * secondValueSplited.toDouble()
             }
+
+            binding.calcResult?.text = tvValueString
+            binding.processCalc?.text = resultSplits?.toString()
+
+
+
+        }catch (e: java.lang.ArithmeticException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun onOperator(view: View) {
+        binding.processCalc?.text?.let {
+            if (lastNumber && !insOperatorIsAdded(it.toString())) {
+                binding.processCalc?.append((view as Button).text)
+                lastDot = false
+                lastNumber = false
+            }
+        }
+    }
+
+    fun insOperatorIsAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") ||
+                    value.contains("*") ||
+                    value.contains("+") ||
+                    value.contains("-")
+        }
+    }
 
 }
