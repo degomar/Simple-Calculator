ac5cde25f65555c8756ea0096396a6c64187a908
Eric <eric.degomar@gmail.com>
2023-02-08 09:17:58 -0300

    apend adicionado e refatorado os metodos clears

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,27 +2,33 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
-    val calcClick = CalculateClickListener()
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //binding = ActivityMainBinding.inflate(layoutInflater)
         binding =  DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-            binding.number0.setOnClickListener {
-            Toast.makeText(this,"sera q foi", Toast.LENGTH_LONG).show()
-
-        }
-
 
     }
+            fun onClickDigit(view: View){
+                binding.processCalc?.append((view as Button).text)
+                Toast.makeText(this, "clicou", Toast.LENGTH_LONG).show()
 
+                }
 
+            fun onClear(view: View) {
+                binding.processCalc?.text = ""
+            }   fun onAllClear(view: View) {
+                onClear(view)
+                binding.calcResult?.text = ""
+            }
 
 }
